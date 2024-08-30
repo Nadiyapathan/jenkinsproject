f4793821a66cb94728c8d8626560dc769030ff00
Nadiyapathan <https://github.com/Nadiyapathan/jenkins-gittask.git>
2024-08-30 10:27:54 +0530

    this is my  secondcommit

---
+++ Calculator.java
@@ -0,0 +1,13 @@
+public class Calculator {
+    public static void main(String[] args) {
+        double num1 = 10.0;
+        double num2 = 6.0;
+
+        double sum = num1 + num2;
+        System.out.println("Addition: " + num1 + " + " + num2 + " = " + sum);
+
+        double difference = num1 - num2;
+        System.out.println("Subtraction: " + num1 + " - " + num2 + " = " + difference);
+
+        double product = num1 * num2;
+        System.out.println("Multiplication: " + num1 + " * " + num2 + " = " + product);
